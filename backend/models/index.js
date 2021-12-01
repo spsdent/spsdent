f2c6640e2c2d03de7d99a2f4af050193a6e42596
Adrian <aperec23@gmail.com>
2021-12-01 16:14:40 +0100

    Import created before models to index file

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,5 +5,9 @@ mongoose.Promise = global.Promise
 const db = {}
 db.mongoose = mongoose
 db.visits = require('./visit')(mongoose)
+db.user = require('./user')
+db.role = require('./role')
+
+db.ROLES = ["user", "admin", "spec"];
 
 module.exports = db
