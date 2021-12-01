0bfb5da78db3ab1191497c2dd9c0f5c4efe08c39
Adrian <aperec23@gmail.com>
2021-12-01 16:15:23 +0100

    Funcion initial help to create rows in roles collection

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,42 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to SPSdent app.' })
 })
 
+const initial = () => {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: 'user',
+      }).save((err) => {
+        if (err) {
+          console.log('error', err)
+        }
+
+        console.log("added 'user' to roles collection")
+      })
+
+      new Role({
+        name: 'admin',
+      }).save((err) => {
+        if (err) {
+          console.log('error', err)
+        }
+
+        console.log("added 'admin' to roles collection")
+      })
+
+      new Role({
+        name: 'spec',
+      }).save((err) => {
+        if (err) {
+          console.log('error', err)
+        }
+
+        console.log("added 'spec' to roles collection")
+      })
+    }
+  })
+}
+
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
