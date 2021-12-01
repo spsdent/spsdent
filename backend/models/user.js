f75eeb0e85be33b4391e26bae1593f6e7bb2dd13
Adrian <aperec23@gmail.com>
2021-12-01 16:14:13 +0100

    Add user model

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.model(
+  'User',
+  new mongoose.Schema({
+    imie: String,
+    nazwisko: String,
+    email: String,
+    telefon: Number,
+    miasto: String,
+    ulica: String,
+    kodPocztowy: Number,
+    email: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Role',
+      },
+    ],
+  })
+)
+
+module.exports = User
