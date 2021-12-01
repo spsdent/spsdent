04681654d95190356bf14c13b8fb654a868777e0
Adrian <aperec23@gmail.com>
2021-12-01 15:50:08 +0100

    Add connect method to server.js

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,33 @@ const cors = require('cors')
 const app = express()
 
 const corsOptions = {
-    origin: 'http://localhost:8081'
+  origin: 'http://localhost:8081',
 }
 
 app.use(cors(corsOptions))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const db = require('./models')
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the database!')
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database!', err)
+    process.exit()
+  })
 
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to SPSdent app.'})
+  res.json({ message: 'Welcome to SPSdent app.' })
 })
 
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
