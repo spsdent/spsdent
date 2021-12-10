4d44623833170ac0e770833a33ec0b543e963f04
maciejsulenta <maciej.sulenta@gmail.com>
2021-12-10 19:29:53 +0100

    fix

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -39,16 +39,16 @@ const Home = () => {
     <PageWrapper>
       <HeaderContainer>
         <TextContainer
-          transition={{ duration: 0.8 }}
+          transition={{ duration: 0.5 }}
           initial={{
             opacity: 0,
-            scale: 0,
+            x: 200,
             translateX: "10%",
             translateY: "10%",
           }}
           animate={{
             opacity: 1,
-            scale: 1,
+            x: 0,
             translateX: "10%",
             translateY: "10%",
           }}
