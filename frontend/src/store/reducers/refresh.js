ea624532d79c8359b03a7f58ee090693a07bd4d9
aprc <aperec23@gmail.com>
2021-12-16 18:47:27 +0100

    Change refresh reducer state

--- frontend/src/store/reducers/refresh.js
+++ frontend/src/store/reducers/refresh.js
@@ -1,14 +1,18 @@
 import { REFRESH_APP } from '../actions/types'
 
-const initialState = { isRefresh: false }
+const initialState = {
+  isRefresh: false,
+}
 
 export default function (state = initialState, action) {
   const { type } = action
 
   switch (type) {
     case REFRESH_APP:
-      return { isRefresh: !state.isRefresh }
-
+      return {
+        ...state,
+        isRefresh: !state.isRefresh,
+      }
     default:
       return state
   }
