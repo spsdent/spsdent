d1584476566aec4b4952861b03fc9c82ded0b8f6
aprc <aperec23@gmail.com>
2021-12-16 18:32:23 +0100

    Create delete service feature

--- frontend/src/pages/ControlPanelPage/DeleteService.js
+++ frontend/src/pages/ControlPanelPage/DeleteService.js
@@ -1,5 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-const DeleteService = () => <h1>Delete service</h1>
+import ServiceData from '../../services/service'
+import { refreshApp } from '../../store/actions/refresh'
+
+const DeleteService = () => {
+  const [servicesArr, setServicesArr] = useState([])
+  const { refresh: isRefresh } = useSelector((state) => state.refresh)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    retrieveServices()
+  }, [isRefresh])
+
+  const retrieveServices = () => {
+    ServiceData.getAll()
+      .then((response) => {
+        console.log(response.data)
+        setServicesArr(response.data)
+      })
+      .catch((e) => console.log(e))
+  }
+
+  const onServiceDelete = (service) => {
+    console.log(service)
+    ServiceData.remove(service)
+      .then((response) => console.log(response.data))
+      .catch((e) => console.log(e))
+    dispatch(refreshApp())
+  }
+
+  return (
+    <>
+      <h1>Usun specjalizacje</h1>
+      {servicesArr.map((service) => (
+        <div
+          style={{
+            width: '300px',
+            backgroundColor: '#333',
+            color: '#fff',
+            marginBottom: '10px',
+            padding: '20px',
+            borderRadius: '10px',
+          }}
+        >
+          <h2>Grupa: {service.grupa}</h2>
+          {service.uslugi.map((usluga) => (
+            <div style={{ margin: '10px 0' }}>
+              <p>Nazwa uslugi: {usluga.nazwa}</p>
+              <p>Cena uslugi: {usluga.cena}</p>
+            </div>
+          ))}
+          <button
+            style={{
+              height: '40px',
+              border: '2px solid #fff',
+              background: 'transparent',
+              fontSize: '14px',
+              cursor: 'pointer',
+              marginTop: '10px',
+              padding: '5px',
+              color: '#fff',
+            }}
+            onClick={() => onServiceDelete(service._id)}
+          >
+            Usun specjalizacje
+          </button>
+        </div>
+      ))}
+    </>
+  )
+}
 
 export default DeleteService
