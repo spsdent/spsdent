78fc82074b9acc4a4778507c9aa72e410c560555
maciejsulenta <maciej.sulenta@gmail.com>
2021-12-17 04:31:53 +0100

    pricelist page

---
+++ frontend/src/pages/PriceListPage/PriceListPageElements.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PriceListPageContainer = styled.div`
+  width: 75%;
+  height: 70vh;
+  background-color: #aaa;
+  margin-top: 3em;
+  font-size: 24px;
+`;
+export const PriceListTitle = styled.h1`
+`;
+export const PriceListTable = styled.div``;
+export const PriceTableTitle = styled.div``;
+export const PriceTableContent = styled.div``;
--- frontend/src/pages/PriceListPage/index.js
+++ frontend/src/pages/PriceListPage/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { PageWrapper } from "../../components/PageWrapper";
+import {
+  PriceListPageContainer,
+  PriceListTitle,
+  PriceListTable,
+  PriceTableTitle,
+  PriceTableContent,
+} from "./PriceListPageElements";
 const PriceListPage = () => {
   return (
     <PageWrapper>
-      <h1>Price list page</h1>
+      <PriceListPageContainer>
+        <PriceListTitle>To co możemy</PriceListTitle>
+        <PriceListTitle>Zaoferować</PriceListTitle>
+        <PriceListTable>
+          <PriceTableTitle></PriceTableTitle>
+          <PriceTableContent></PriceTableContent>
+        </PriceListTable>
+      </PriceListPageContainer>
     </PageWrapper>
   );
 };
