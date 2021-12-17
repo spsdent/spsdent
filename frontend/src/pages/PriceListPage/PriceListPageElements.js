0b95e23ad1f8abcb702bffae064abdcf9d78b817
maciejsulenta <maciej.sulenta@gmail.com>
2021-12-17 05:42:12 +0100

    pricelist page table

--- frontend/src/pages/PriceListPage/PriceListPageElements.js
+++ frontend/src/pages/PriceListPage/PriceListPageElements.js
@@ -3,12 +3,39 @@ import styled from "styled-components";
 export const PriceListPageContainer = styled.div`
   width: 75%;
   height: 70vh;
-  background-color: #aaa;
+  /* background-color: #aaa; */
   margin-top: 3em;
   font-size: 24px;
 `;
 export const PriceListTitle = styled.h1`
+  color: ${(props) => (props.primary ? "#01d4bf" : "#333")};
+  font-size: ${(props) => (props.primary ? "2.5em" : "3.5em")};
+  line-height: 1em;
+  letter-spacing: 0.05em;
 `;
-export const PriceListTable = styled.div``;
-export const PriceTableTitle = styled.div``;
-export const PriceTableContent = styled.div``;
+export const PriceListTable = styled.div`
+  color: #333;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  height: fit-content;
+  font-family: "montserrat";
+  box-shadow: 0px 1px 5px 1px #333;
+`;
+export const PriceTableRow = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: space-around;
+  background-color: ${(props) => (props.primary ? "#333" : "#fff")};
+  color: ${(props) => (props.primary ? "#fff" : "#333")};
+  width: 100%;
+  font-size: ${(props) => (props.primary ? "1em" : ".8em")};
+  border-bottom: 2px solid rgba(51,51,51,0.2);
+  padding: 1em 0;
+`;
+export const PriceTableContent = styled.div`
+  display: flex;
+  justify-content: ${(props) => (props.price ? "center" : "flex-start")};
+  width: 40%; 
+`;
--- frontend/src/pages/PriceListPage/index.js
+++ frontend/src/pages/PriceListPage/index.js
@@ -4,18 +4,36 @@ import {
   PriceListPageContainer,
   PriceListTitle,
   PriceListTable,
-  PriceTableTitle,
+  PriceTableRow,
   PriceTableContent,
 } from "./PriceListPageElements";
 const PriceListPage = () => {
   return (
     <PageWrapper>
       <PriceListPageContainer>
-        <PriceListTitle>To co możemy</PriceListTitle>
-        <PriceListTitle>Zaoferować</PriceListTitle>
+        <PriceListTitle primary>cennik</PriceListTitle>
+        <PriceListTitle>Stomatologia zachowawcza</PriceListTitle>
         <PriceListTable>
-          <PriceTableTitle></PriceTableTitle>
-          <PriceTableContent></PriceTableContent>
+          <PriceTableRow primary>
+          <PriceTableContent>Usługa</PriceTableContent>
+          <PriceTableContent price>Cena</PriceTableContent>
+          </PriceTableRow>
+          <PriceTableRow>
+          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
+          <PriceTableContent price>50 zł</PriceTableContent>
+          </PriceTableRow>
+          <PriceTableRow>
+          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
+          <PriceTableContent price>50 zł</PriceTableContent>
+          </PriceTableRow>
+          <PriceTableRow>
+          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
+          <PriceTableContent price>50 zł</PriceTableContent>
+          </PriceTableRow>
+          <PriceTableRow>
+          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
+          <PriceTableContent price>50 zł</PriceTableContent>
+          </PriceTableRow>
         </PriceListTable>
       </PriceListPageContainer>
     </PageWrapper>
