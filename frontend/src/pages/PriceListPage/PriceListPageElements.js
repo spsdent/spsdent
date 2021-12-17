dadacf8353d9c02637b619a3ce3c48eb5b06a9bd
maciejsulenta <maciej.sulenta@gmail.com>
2021-12-17 07:36:19 +0100

    pricelist page animations

--- frontend/src/pages/PriceListPage/PriceListPageElements.js
+++ frontend/src/pages/PriceListPage/PriceListPageElements.js
@@ -1,19 +1,33 @@
 import styled from "styled-components";
-
+import { motion } from "framer-motion";
 export const PriceListPageContainer = styled.div`
   width: 75%;
   height: 70vh;
   /* background-color: #aaa; */
   margin-top: 3em;
   font-size: 24px;
+  @media screen and (max-width: 1500px) {
+    font-size: 20px;
+  }
+  @media screen and (max-width: 1280px) {
+    font-size: 18px;
+  }
+  @media screen and (max-width: 960px) {
+    font-size: 16px;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
+  }
 `;
-export const PriceListTitle = styled.h1`
+export const PriceListTitle = styled(motion.h1)`
   color: ${(props) => (props.primary ? "#01d4bf" : "#333")};
   font-size: ${(props) => (props.primary ? "2.5em" : "3.5em")};
   line-height: 1em;
   letter-spacing: 0.05em;
+  z-index: 1;
 `;
-export const PriceListTable = styled.div`
+export const PriceListTable = styled(motion.div)`
+  margin-top: 1.5em;
   color: #333;
   display: flex;
   justify-content: center;
@@ -21,9 +35,9 @@ export const PriceListTable = styled.div`
   align-items: flex-start;
   height: fit-content;
   font-family: "montserrat";
-  box-shadow: 0px 1px 5px 1px #333;
+  /* box-shadow: 0px 1px 10px 1px rgba(51, 51, 51, 0.2); */
 `;
-export const PriceTableRow = styled.div`
+export const PriceTableRow = styled(motion.div)`
   display: flex;
   justify-content: space-around;
   align-items: space-around;
@@ -31,11 +45,18 @@ export const PriceTableRow = styled.div`
   color: ${(props) => (props.primary ? "#fff" : "#333")};
   width: 100%;
   font-size: ${(props) => (props.primary ? "1em" : ".8em")};
-  border-bottom: 2px solid rgba(51,51,51,0.2);
-  padding: 1em 0;
+  border-left: 2px solid rgba(51, 51, 51, 0.2);
+  border-right: 2px solid rgba(51, 51, 51, 0.2);
+  border-bottom: 2px solid rgba(51, 51, 51, 0.2);
+  padding: 1.2em 0;
+  transition: 0.1s linear;
+  z-index: ${props => props.primary ? "2" : "1"};
+  &:hover {
+    background-color: ${(props) => (props.primary ? "#333" : "#f6f6f6")};
+  }
 `;
 export const PriceTableContent = styled.div`
   display: flex;
   justify-content: ${(props) => (props.price ? "center" : "flex-start")};
-  width: 40%; 
+  width: ${(props) => (props.price ? "25%" : "55%")};
 `;
--- frontend/src/pages/PriceListPage/index.js
+++ frontend/src/pages/PriceListPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PageWrapper } from "../../components/PageWrapper";
+import { Pattern } from "../../components/Pattern";
 import {
   PriceListPageContainer,
   PriceListTitle,
@@ -8,34 +9,103 @@ import {
   PriceTableContent,
 } from "./PriceListPageElements";
 const PriceListPage = () => {
+  const container = {
+    hidden: { y: -100, opacity: 0, scale: 1 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.3,
+        delayChildren: 0.3,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <PageWrapper>
       <PriceListPageContainer>
-        <PriceListTitle primary>cennik</PriceListTitle>
-        <PriceListTitle>Stomatologia zachowawcza</PriceListTitle>
-        <PriceListTable>
+        <PriceListTitle
+          primary
+          transition={{ type: "spring", bounce: 0.5, duration: 1.2 }}
+          initial={{ y: -200 }}
+          animate={{ y: 0 }}
+        >
+          cennik
+        </PriceListTitle>
+        <PriceListTitle
+          transition={{ type: "spring", bounce: 0.5, duration: 1.5 }}
+          initial={{ y: -200 }}
+          animate={{ y: 0 }}
+        >
+          Stomatologia zachowawcza
+        </PriceListTitle>
+        <PriceListTable variants={container} initial="hidden" animate="visible">
           <PriceTableRow primary>
-          <PriceTableContent>Usługa</PriceTableContent>
-          <PriceTableContent price>Cena</PriceTableContent>
+            <PriceTableContent>Usługa</PriceTableContent>
+            <PriceTableContent price>Cena</PriceTableContent>
           </PriceTableRow>
-          <PriceTableRow>
-          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
-          <PriceTableContent price>50 zł</PriceTableContent>
+          <PriceTableRow variants={item}>
+            <PriceTableContent>
+              Znieczulenie komputerowe Wand STA
+            </PriceTableContent>
+            <PriceTableContent price>50 zł</PriceTableContent>
           </PriceTableRow>
-          <PriceTableRow>
-          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
-          <PriceTableContent price>50 zł</PriceTableContent>
+          <PriceTableRow variants={item}>
+            <PriceTableContent>Konsultacja specjalistyczna</PriceTableContent>
+            <PriceTableContent price>100 zł</PriceTableContent>
           </PriceTableRow>
-          <PriceTableRow>
-          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
-          <PriceTableContent price>50 zł</PriceTableContent>
+          <PriceTableRow variants={item}>
+            <PriceTableContent>
+              Wypełnienie światłoutwardzalne
+            </PriceTableContent>
+            <PriceTableContent price>200 zł</PriceTableContent>
           </PriceTableRow>
-          <PriceTableRow>
-          <PriceTableContent>Znieczulenie komputerowe Wand STA</PriceTableContent>
-          <PriceTableContent price>50 zł</PriceTableContent>
+          <PriceTableRow variants={item}>
+            <PriceTableContent>
+              Rekonstrukcja na ćwieku metalowym
+            </PriceTableContent>
+            <PriceTableContent price>250 zł</PriceTableContent>
+          </PriceTableRow>
+          <PriceTableRow variants={item}>
+            <PriceTableContent>
+              Rekonstrukcja zęba na wkładzie z włókien szklanych
+            </PriceTableContent>
+            <PriceTableContent price>450 zł</PriceTableContent>
+          </PriceTableRow>
+          <PriceTableRow variants={item}>
+            <PriceTableContent>
+              Znieczulenie komputerowe Wand STA
+            </PriceTableContent>
+            <PriceTableContent price>50 zł</PriceTableContent>
           </PriceTableRow>
         </PriceListTable>
       </PriceListPageContainer>
+      <Pattern
+        src="Pattern.png"
+        top={"45%"}
+        left={"5%"}
+        transition={{ type: "spring", bounce: 0.5, duration: 2, delay: 0.5 }}
+        initial={{ opacity: 0, x: -200, rotate: 90 }}
+        animate={{ opacity: 1, x: 0, rotate: 90 }}
+      />
+      <Pattern
+        src="Pattern.png"
+        top={"5%"}
+        left={"70%"}
+        transition={{ type: "spring", bounce: 0.5, duration: 2, delay: 0.5 }}
+        initial={{ opacity: 0, y: -200 }}
+        animate={{ opacity: 1, y: 0 }}
+      />
     </PageWrapper>
   );
 };
