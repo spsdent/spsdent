475fa40d72a43113f951147d5e4564759a5c33fc
maciejsulenta <maciej.sulenta@gmail.com>
2021-12-21 12:58:00 +0100

    timesheet page animations

--- frontend/src/pages/DoctorTimesheetPage/TimesheetPageElements.js
+++ frontend/src/pages/DoctorTimesheetPage/TimesheetPageElements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const TimesheetPageContainer = styled.div`
   width: 80%;
@@ -9,15 +10,15 @@ export const TimesheetPageContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  /* @media screen and (max-width: 1500px) {
+  @media screen and (max-width: 1500px) {
     font-size: 18px;
   }
   @media screen and (max-width: 1280px) {
     font-size: 16px;
   }
   @media screen and (max-width: 960px) {
-    font-size: 13px;
-  } */
+    font-size: 14px;
+  }
 `;
 
 export const TimesheetTitleContainer = styled.div`
@@ -26,12 +27,16 @@ export const TimesheetTitleContainer = styled.div`
   height: 15%;
   font-family: "bebas neue";
 `;
-export const TimesheetTitle = styled.h1`
+export const TimesheetTitle = styled(motion.h1)`
   color: ${(props) => (props.primary ? "#01d4bf" : "#333")};
   display: inline-block;
   padding-left: 0.3em;
   font-size: 4em;
   letter-spacing: 0.05em;
+  @media screen and (max-width: 768px) {
+      font-size: 2.5em;
+       
+  }
 `;
 export const TimesheetPickContainer = styled.div`
   width: 100%;
@@ -41,7 +46,7 @@ export const TimesheetPickContainer = styled.div`
   height: 15%;
   /* background-color: #999; */
 `;
-export const TimesheetPick = styled.input`
+export const TimesheetPick = styled(motion.select)`
   width: 47%;
   padding: 0.6em;
   outline: none;
@@ -49,6 +54,7 @@ export const TimesheetPick = styled.input`
   font-family: "bebas neue";
   font-size: 0.7em;
   color: #333;
+  letter-spacing: .05em;
   &:focus {
     box-shadow: 0px 0px 3px 0.1px #333;
     outline: none;
@@ -71,7 +77,7 @@ export const TimesheetContainer = styled.div`
   /* background-color: #666; */
 `;
 
-export const TimesheetDaysContainer = styled.div`
+export const TimesheetDaysContainer = styled(motion.div)`
   width: 92%;
   height: 10%;
   /* background-color: cadetblue; */
@@ -94,7 +100,7 @@ export const TimesheetWrap = styled.div`
   /* background-color: gray; */
   display: flex;
 `;
-export const TimesheetHoursContainer = styled.div`
+export const TimesheetHoursContainer = styled(motion.div)`
   width: 8%;
   /* min-height: 100%; */
   height: fit-content;
@@ -116,13 +122,13 @@ export const Hour = styled.p`
   /* background-color: brown; */
 `;
 
-export const Timesheet = styled.div`
+export const Timesheet = styled(motion.div)`
   width: 92%;
   height: 100%;
   /* background-color: #424; */
 `;
 
-export const VisitRow = styled.div`
+export const VisitRow = styled(motion.div)`
   width: 100%;
   height: 3em;
   /* background-color: gray; */
@@ -132,14 +138,15 @@ export const VisitRow = styled.div`
   align-items: center;
 `;
 
-export const Visit = styled.div`
+export const Visit = styled(motion.div)`
   width: 15%;
   height: 2.5em;
   border-radius: 10px;
   background-color: ${(props) => (props.available ? "#c1c1c1" : "#333")};
+  cursor: pointer;
 `;
 export const TimesheetLegend = styled.div`
-  width: 75%;
+  width: 80%;
   height: 2em;
   position: fixed;
   bottom: 0;
@@ -149,33 +156,37 @@ export const TimesheetLegend = styled.div`
   align-items: center;
 `;
 export const LegendItemWrap = styled.div`
-display: flex;
-align-items: center;
-justify-content: flex-start;
-width: fit-content;
-padding-right: 2em;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: fit-content;
+  padding-right: 2em;
 `;
-export const CircleFree = styled.div`
+export const CircleFree = styled(motion.div)`
   width: 0.5em;
   height: 0.5em;
   border-radius: 50%;
   background-color: #333;
 `;
-export const CircleReserved = styled.div`
+export const CircleReserved = styled(motion.div)`
   width: 0.5em;
   height: 0.5em;
   border-radius: 50%;
   background-color: #c1c1c1;
 `;
-export const CircleActive = styled.div`
+export const CircleActive = styled(motion.div)`
   width: 0.5em;
   height: 0.5em;
   border-radius: 50%;
   background-color: #01d4bf;
 `;
-export const LegendText = styled.p`
+export const LegendText = styled(motion.p)`
   color: #333;
   font-family: "poppins";
-  font-size: 0.7em;
-  margin-left: 1em;
+  font-size: 0.6rem;
+  margin-left: 0.5em;
+  white-space: nowrap;
+  @media screen and (max-width: 768px) {
+    font-size: 0.4em;
+  }
 `;
--- frontend/src/pages/DoctorTimesheetPage/index.js
+++ frontend/src/pages/DoctorTimesheetPage/index.js
@@ -25,189 +25,315 @@ import {
   LegendText,
 } from "./TimesheetPageElements";
 const DoctorTimesheetPage = () => {
-  return (
-    <PageWrapper>
-      <TimesheetPageContainer>
-        <TimesheetTitleContainer>
-          <TimesheetTitle>Zarezerwuj</TimesheetTitle>
-          <TimesheetTitle primary>Wizytę</TimesheetTitle>
-        </TimesheetTitleContainer>
-        <TimesheetPickContainer>
-          <TimesheetPick as="select">
-            <Option value="" disabled selected hidden>
-              Wybierz usługę
-            </Option>
-            <Option value="1">najak</Option>
-          </TimesheetPick>
-          <TimesheetPick as="select">
-            <Option value="" disabled selected hidden>
-              Wybierz specjalistę
-            </Option>
-            <Option value="2">esa?</Option>
-          </TimesheetPick>
-        </TimesheetPickContainer>
+  const container = {
+    hidden: {},
+    visible: {
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.15,
+      },
+    },
+  };
 
-        <TimesheetContainer>
-          <TimesheetDaysContainer>
-            <Day>poniedziałek</Day>
-            <Day>wtorek</Day>
-            <Day>środa</Day>
-            <Day>czwartek</Day>
-            <Day>piątek</Day>
-          </TimesheetDaysContainer>
-          <TimesheetWrap>
-            <TimesheetHoursContainer>
-              <Hour>8:00</Hour>
-              <Hour>8:30</Hour>
-              <Hour>9:00</Hour>
-              <Hour>9:30</Hour>
-              <Hour>10:00</Hour>
-              <Hour>10:30</Hour>
-              <Hour>11:00</Hour>
-              <Hour>11:30</Hour>
-              <Hour>12:00</Hour>
-              <Hour>12:30</Hour>
-              <Hour>13:00</Hour>
-              <Hour>13:30</Hour>
-              <Hour>14:00</Hour>
-              <Hour>14:30</Hour>
-              <Hour>15:00</Hour>
-              <Hour>15:30</Hour>
-              <Hour>16:00</Hour>
-            </TimesheetHoursContainer>
-            <Timesheet>
-              <VisitRow>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit available></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit available></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-              </VisitRow>
-              <VisitRow>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit></Visit>
-                <Visit available></Visit>
-                <Visit></Visit>
-              </VisitRow>
-            </Timesheet>
-          </TimesheetWrap>
-        </TimesheetContainer>
-      </TimesheetPageContainer>
-      <TimesheetLegend>
-        <LegendItemWrap>
-          <CircleFree />
-          <LegendText>Wolny termin</LegendText>
-        </LegendItemWrap>
-        <LegendItemWrap>
-          <CircleReserved />
-          <LegendText>Zarezerwowany termin</LegendText>
-        </LegendItemWrap>
-        <LegendItemWrap>
-          <CircleActive />
-          <LegendText>Zaznaczony termin</LegendText>
-        </LegendItemWrap>
-      </TimesheetLegend>
-      <Pattern src="Pattern.png" />
-    </PageWrapper>
+  const itemOne = {
+    hidden: { x: 100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { type: "spring", duration: 0.5, damping: 7, stiffness: 50 },
+    },
+  };
+  const itemTwo = {
+    hidden: { x: -100, opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+  };
+
+  return (
+    <>
+      <PageWrapper>
+        <TimesheetPageContainer>
+          <TimesheetTitleContainer>
+            <TimesheetTitle
+              initial={{ x: 300, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                type: "spring",
+                damping: 7,
+                stiffness: 50,
+                duration: 0.5,
+              }}
+            >
+              Zarezerwuj
+            </TimesheetTitle>
+            <TimesheetTitle
+              primary
+              initial={{ x: 300, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                type: "spring",
+                damping: 7,
+                stiffness: 50,
+                duration: 0.5,
+                delay: 0.3,
+              }}
+            >
+              Wizytę
+            </TimesheetTitle>
+          </TimesheetTitleContainer>
+          <TimesheetPickContainer>
+            <TimesheetPick
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Option value="" disabled selected hidden>
+                Wybierz usługę
+              </Option>
+              <Option value="1">najak</Option>
+            </TimesheetPick>
+            <TimesheetPick
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              <Option value="" disabled selected hidden>
+                Wybierz specjalistę
+              </Option>
+              <Option value="2">esa?</Option>
+            </TimesheetPick>
+          </TimesheetPickContainer>
+
+          <TimesheetContainer>
+            <TimesheetDaysContainer
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Day>poniedziałek</Day>
+              <Day>wtorek</Day>
+              <Day>środa</Day>
+              <Day>czwartek</Day>
+              <Day>piątek</Day>
+            </TimesheetDaysContainer>
+            <TimesheetWrap>
+              <TimesheetHoursContainer
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Hour>8:00</Hour>
+                <Hour>8:30</Hour>
+                <Hour>9:00</Hour>
+                <Hour>9:30</Hour>
+                <Hour>10:00</Hour>
+                <Hour>10:30</Hour>
+                <Hour>11:00</Hour>
+                <Hour>11:30</Hour>
+                <Hour>12:00</Hour>
+                <Hour>12:30</Hour>
+                <Hour>13:00</Hour>
+                <Hour>13:30</Hour>
+                <Hour>14:00</Hour>
+                <Hour>14:30</Hour>
+                <Hour>15:00</Hour>
+                <Hour>15:30</Hour>
+                <Hour>16:00</Hour>
+              </TimesheetHoursContainer>
+
+              <Timesheet
+                variants={container}
+                initial="hidden"
+                animate="visible"
+              >
+                <VisitRow variants={itemOne}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemOne}>
+                  <Visit available></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemOne}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemOne}>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemOne}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemOne}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemOne}>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemOne}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+                <VisitRow variants={itemTwo}>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit></Visit>
+                  <Visit available></Visit>
+                  <Visit></Visit>
+                </VisitRow>
+              </Timesheet>
+            </TimesheetWrap>
+          </TimesheetContainer>
+        </TimesheetPageContainer>
+
+        <Pattern
+          initial={{ x: 300, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            damping: 7,
+            stiffness: 50,
+            duration: 0.5,
+            delay: 0.6,
+          }}
+          src="Pattern.png"
+          top="15%"
+          left="70%"
+        />
+        <TimesheetLegend>
+          <LegendItemWrap>
+            <CircleFree
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                type: "spring",
+                damping: 3,
+                stiffness: 150,
+              }}
+            />
+            <LegendText
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              Wolny termin
+            </LegendText>
+          </LegendItemWrap>
+          <LegendItemWrap>
+            <CircleReserved
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                type: "spring",
+                damping: 3,
+                stiffness: 150,
+              }}
+            />
+            <LegendText
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              Zarezerwowany termin
+            </LegendText>
+          </LegendItemWrap>
+          <LegendItemWrap>
+            <CircleActive
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                type: "spring",
+                damping: 3,
+                stiffness: 150,
+              }}
+            />
+            <LegendText
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              Zaznaczony termin
+            </LegendText>
+          </LegendItemWrap>
+        </TimesheetLegend>
+      </PageWrapper>
+    </>
   );
 };
 
