0e413eeb6227b4cd3e322621cda0c6844a1c7299
aprc <aperec23@gmail.com>
2021-12-29 18:15:55 +0100

    Change div positon

--- frontend/src/pages/ArchiveVisitsPage/index.js
+++ frontend/src/pages/ArchiveVisitsPage/index.js
@@ -74,74 +74,77 @@ const ArchiveVisitsList = () => {
           <Title>Archiwum </Title>
           <Title primary>wizyt</Title>
         </TitleContainer>
-        <div
-          style={{
-            width: '95%',
-            display: 'grid',
-            gridTemplateColumns: '350px 2fr 1fr 1fr 1fr 1fr',
-            columnGap: '30px',
-            padding: '.5rem',
-            margin: '0 auto',
-          }}
-        >
-          <p>Usluga</p>
-          <p>Lekarz</p>
-          <p>Data</p>
-          <p>Godzina</p>
-          <p>Cena</p>
-        </div>
+
         {visitsList.length > 0 ? (
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            {visitsList.map((item) => (
-              <div
-                style={{
-                  width: '95%',
-                  backgroundColor: '#FFFFFF',
-                  color: 'rgba(75,75,75,.75)',
-                  padding: '.5rem',
-                  marginBottom: '.5rem',
-                  cursor: 'pointer',
-                  borderRadius: '.75rem',
-                  fontFamily: 'Poppins',
-                  fontWeight: '500',
-                  display: 'grid',
-                  gridTemplateColumns: '350px 2fr 1fr 1fr 1fr 1fr',
-                  columnGap: '30px',
-                  justifyContent: 'space-around',
-                  alignItems: 'center',
-                }}
-                onClick={() => goToVisit(item)}
-              >
-                <p>{item.usluga}</p>
-                <p>Leno Paleno</p>
-                <p>{item.data}</p>
-                <p>{item.godzina}</p>
-                <p>{item.cena}zl</p>
-                <button
-                  onClick={() => deleteVisit(item)}
+          <>
+            <div
+              style={{
+                width: '95%',
+                display: 'grid',
+                gridTemplateColumns: '350px 2fr 1fr 1fr 1fr 1fr',
+                columnGap: '30px',
+                padding: '.5rem',
+                margin: '0 auto',
+              }}
+            >
+              <p>Usluga</p>
+              <p>Lekarz</p>
+              <p>Data</p>
+              <p>Godzina</p>
+              <p>Cena</p>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              {visitsList.map((item) => (
+                <div
                   style={{
-                    backgroundColor: 'transparent',
-                    border: '2px solid #333',
-                    color: '#333',
-                    padding: '5px',
-                    marginRight: '5px',
+                    width: '95%',
+                    backgroundColor: '#FFFFFF',
+                    color: 'rgba(75,75,75,.75)',
+                    padding: '.5rem',
+                    marginBottom: '.5rem',
                     cursor: 'pointer',
-                    width: '30px',
-                    height: '30px',
-                    borderRadius: '30px',
+                    borderRadius: '.75rem',
+                    fontFamily: 'Poppins',
+                    fontWeight: '500',
+                    display: 'grid',
+                    gridTemplateColumns: '350px 2fr 1fr 1fr 1fr 1fr',
+                    columnGap: '30px',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
                   }}
+                  onClick={() => goToVisit(item)}
                 >
-                  X
-                </button>
-              </div>
-            ))}
-          </div>
+                  <p>{item.usluga}</p>
+                  <p>Leno Paleno</p>
+                  <p>{item.data}</p>
+                  <p>{item.godzina}</p>
+                  <p>{item.cena}zl</p>
+                  <button
+                    onClick={() => deleteVisit(item)}
+                    style={{
+                      backgroundColor: 'transparent',
+                      border: '2px solid #333',
+                      color: '#333',
+                      padding: '5px',
+                      marginRight: '5px',
+                      cursor: 'pointer',
+                      width: '30px',
+                      height: '30px',
+                      borderRadius: '30px',
+                    }}
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
+            </div>
+          </>
         ) : (
           <EmptyListHeading>Brak archiwalnych wizyt</EmptyListHeading>
         )}
