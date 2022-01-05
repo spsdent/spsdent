10b02f923fc2977af6096d2badcc4516cc88202b
aprc <aperec23@gmail.com>
2022-01-05 14:19:01 +0100

    Add visits page styles to archive visits page, and slightly modify component structure

--- frontend/src/pages/ArchiveVisitsPage/index.js
+++ frontend/src/pages/ArchiveVisitsPage/index.js
@@ -5,13 +5,22 @@ import { refreshApp } from '../../store/actions/refresh'
 import { useNavigate } from 'react-router-dom'
 import useDebounce from '../../hooks/useDebounce'
 import { PageWrapper } from '../../components/PageWrapper'
-
+import { FaTrashAlt } from 'react-icons/fa'
+import { Pattern } from '../../components/Pattern'
 import {
-  Title,
-  Container,
-  TitleContainer,
-  EmptyListHeading,
-} from './ArchiveVisitsPageElements'
+  VisitsPageContainer,
+  VisitsPageTitleContainer,
+  VisitsPageTitle,
+  VisitsContainer,
+  Headers,
+  Header,
+  HeaderText,
+  Triangle,
+  VisitsListContainer,
+  Visit,
+  VisitContent,
+  VisitDelete,
+} from '../VisitsPage/VisitsPageElements'
 
 const ArchiveVisitsList = () => {
   const [visitsList, setVisitsList] = useState([])
@@ -67,88 +76,118 @@ const ArchiveVisitsList = () => {
     navigate(`/visits/${item.id}`, { state: item })
   }
 
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delayChildren: 1,
+        staggerChildren: 1,
+      },
+    },
+  }
+  const itemOne = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  }
+
   return (
     <PageWrapper>
-      <Container>
-        <TitleContainer>
-          <Title>Archiwum </Title>
-          <Title primary>wizyt</Title>
-        </TitleContainer>
-
-        {visitsList.length > 0 ? (
-          <>
-            <div
-              style={{
-                width: '95%',
-                display: 'grid',
-                gridTemplateColumns: '350px 2fr 1fr 1fr 1fr 1fr',
-                columnGap: '30px',
-                padding: '.5rem',
-                margin: '0 auto',
-              }}
-            >
-              <p>Usluga</p>
-              <p>Lekarz</p>
-              <p>Data</p>
-              <p>Godzina</p>
-              <p>Cena</p>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              {visitsList.map((item) => (
-                <div
-                  style={{
-                    width: '95%',
-                    backgroundColor: '#FFFFFF',
-                    color: 'rgba(75,75,75,.75)',
-                    padding: '.5rem',
-                    marginBottom: '.5rem',
-                    cursor: 'pointer',
-                    borderRadius: '.75rem',
-                    fontFamily: 'Poppins',
-                    fontWeight: '500',
-                    display: 'grid',
-                    gridTemplateColumns: '350px 2fr 1fr 1fr 1fr 1fr',
-                    columnGap: '30px',
-                    justifyContent: 'space-around',
-                    alignItems: 'center',
-                  }}
-                  onClick={() => goToVisit(item)}
-                >
-                  <p>{item.usluga}</p>
-                  <p>Leno Paleno</p>
-                  <p>{item.data}</p>
-                  <p>{item.godzina}</p>
-                  <p>{item.cena}zl</p>
-                  <button
-                    onClick={() => deleteVisit(item)}
-                    style={{
-                      backgroundColor: 'transparent',
-                      border: '2px solid #333',
-                      color: '#333',
-                      padding: '5px',
-                      marginRight: '5px',
-                      cursor: 'pointer',
-                      width: '30px',
-                      height: '30px',
-                      borderRadius: '30px',
-                    }}
+      <VisitsPageContainer>
+        <VisitsPageTitleContainer>
+          <VisitsPageTitle
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.1 }}
+          >
+            Archiwum
+          </VisitsPageTitle>
+          <VisitsPageTitle
+            primary
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.2 }}
+          >
+            Rezerwacji
+          </VisitsPageTitle>
+        </VisitsPageTitleContainer>
+        <VisitsContainer>
+          {[].length > 0 ? (
+            <>
+              <Headers variants={container} initial='hidden' animate='show'>
+                <Header primary>
+                  <HeaderText variants={itemOne}>usługa</HeaderText>{' '}
+                  <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>lekarz</HeaderText> <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>data</HeaderText> <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>godzina</HeaderText> <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>cena</HeaderText> <Triangle />
+                </Header>
+              </Headers>
+              <VisitsListContainer
+                variants={container}
+                initial='hidden'
+                animate='show'
+              >
+                {[].map((item, i) => (
+                  <Visit
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.3, delay: i * 0.2 }}
+                    key={item._id}
+                    onClick={() => goToVisit(item)}
                   >
-                    X
-                  </button>
-                </div>
-              ))}
-            </div>
-          </>
-        ) : (
-          <EmptyListHeading>Brak archiwalnych wizyt</EmptyListHeading>
-        )}
-      </Container>
+                    <VisitContent primary>{item.usluga}</VisitContent>
+                    <VisitContent>Leno Paleno</VisitContent>
+                    <VisitContent>{item.data}</VisitContent>
+                    <VisitContent>{item.godzina}:00</VisitContent>
+                    <VisitContent>{item.cena}zł</VisitContent>
+                    <VisitDelete onClick={() => deleteVisit(item)}>
+                      <FaTrashAlt />
+                    </VisitDelete>
+                  </Visit>
+                ))}
+              </VisitsListContainer>
+            </>
+          ) : (
+            <VisitsPageTitle
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.1 }}
+            >
+              Brak archiwalnych wizyt
+            </VisitsPageTitle>
+          )}
+        </VisitsContainer>
+      </VisitsPageContainer>
+      <Pattern
+        src='Pattern.png'
+        top={'12%'}
+        left={'70%'}
+        transition={{ duration: 0.5 }}
+        initial={{ y: -400 }}
+        animate={{ y: 0 }}
+      />
+      <Pattern
+        src='Pattern.png'
+        top={'70%'}
+        left={'13%'}
+        transition={{ duration: 1 }}
+        initial={{ y: 400 }}
+        animate={{ y: 0 }}
+      />
     </PageWrapper>
   )
 }
--- frontend/src/pages/VisitsPage/index.js
+++ frontend/src/pages/VisitsPage/index.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import VisitDataService from "../../services/visit";
-import { useSelector, useDispatch } from "react-redux";
-import { refreshApp } from "../../store/actions/refresh";
-import { useNavigate } from "react-router-dom";
-import { PageWrapper } from "../../components/PageWrapper";
-import { FaTrashAlt } from "react-icons/fa";
-import { Pattern } from "../../components/Pattern";
+import React, { useEffect, useState } from 'react'
+import VisitDataService from '../../services/visit'
+import { useSelector, useDispatch } from 'react-redux'
+import { refreshApp } from '../../store/actions/refresh'
+import { useNavigate } from 'react-router-dom'
+import { PageWrapper } from '../../components/PageWrapper'
+import { FaTrashAlt } from 'react-icons/fa'
+import { Pattern } from '../../components/Pattern'
 import {
   VisitsPageContainer,
   VisitsPageTitleContainer,
@@ -19,138 +19,173 @@ import {
   Visit,
   VisitContent,
   VisitDelete,
-} from "./VisitsPageElements";
+} from './VisitsPageElements'
 
 const VisitsPage = () => {
-  const [visitsList, setVisitsList] = useState([]);
-  const { user: currentUser } = useSelector((state) => state.auth);
-  const { refresh: isRefresh } = useSelector((state) => state);
-  const dispatch = useDispatch();
-  let navigate = useNavigate();
+  const [visitsList, setVisitsList] = useState([])
+  const { user: currentUser } = useSelector((state) => state.auth)
+  const { refresh: isRefresh } = useSelector((state) => state)
+  const dispatch = useDispatch()
+  let navigate = useNavigate()
 
   useEffect(() => {
     if (currentUser) {
-      currentUser.roles.includes("ROLE_SPEC");
-      currentUser.roles.includes("ROLE_ADMIN");
+      currentUser.roles.includes('ROLE_SPEC')
+      currentUser.roles.includes('ROLE_ADMIN')
     }
-  }, [currentUser]);
+  }, [currentUser])
 
   useEffect(() => {
-    retrieveVisits();
-  }, [isRefresh]);
+    retrieveVisits()
+  }, [isRefresh])
 
   const retrieveVisits = () => {
     VisitDataService.getAll()
       .then((response) => {
-        const visitsArr = response.data.filter((item) => item.status === false);
+        const visitsArr = response.data.filter((item) => item.status === false)
         if (
-          currentUser.roles.includes("ROLE_ADMIN") ||
-          currentUser.roles.includes("ROLE_SPEC")
+          currentUser.roles.includes('ROLE_ADMIN') ||
+          currentUser.roles.includes('ROLE_SPEC')
         ) {
-          setVisitsList(visitsArr);
+          setVisitsList(visitsArr)
         } else {
           const userVisitsArr = response.data.filter(
             (visit) => visit.uid === currentUser.id
-          );
-          setVisitsList(userVisitsArr);
+          )
+          setVisitsList(userVisitsArr)
         }
       })
       .catch((e) => {
-        console.log(e);
-      });
-  };
+        console.log(e)
+      })
+  }
 
   const deleteVisit = (item) => {
     VisitDataService.remove(item.id)
       .then((response) => {
-        console.log("Usunieto wizyte pomyslnie!");
-        dispatch(refreshApp());
+        console.log('Usunieto wizyte pomyslnie!')
+        dispatch(refreshApp())
       })
-      .catch((e) => console.log(e));
-  };
+      .catch((e) => console.log(e))
+  }
 
   const goToVisit = (item) => {
-    navigate(`/visits/${item.id}`, { state: item });
-  };
+    navigate(`/visits/${item.id}`, { state: item })
+  }
 
-const container = {
-  hidden: {opacity: 0},
-  show: {
-    opacity: 1,
-    transition: {
-      duration: .5,
-      delayChildren: 1,
-      staggerChildren: 1,
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delayChildren: 1,
+        staggerChildren: 1,
+      },
     },
-  },
-}
-const itemOne = {
-  hidden: { y: -100, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
-}
+  }
+  const itemOne = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  }
   return (
     <PageWrapper>
       <VisitsPageContainer>
         <VisitsPageTitleContainer>
-          <VisitsPageTitle initial={{y: -50, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{delay: .1}}>Aktualne</VisitsPageTitle>
-          <VisitsPageTitle primary initial={{y: -50, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{delay: .2}}>Rezerwacje</VisitsPageTitle>
+          <VisitsPageTitle
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.1 }}
+          >
+            Aktualne
+          </VisitsPageTitle>
+          <VisitsPageTitle
+            primary
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.2 }}
+          >
+            Rezerwacje
+          </VisitsPageTitle>
         </VisitsPageTitleContainer>
         <VisitsContainer>
-          <Headers variants={container} initial="hidden" animate="show">
-            <Header primary>
-              <HeaderText variants={itemOne}>usługa</HeaderText> <Triangle />
-            </Header>
-            <Header>
-              <HeaderText>lekarz</HeaderText> <Triangle />
-            </Header>
-            <Header>
-              <HeaderText>data</HeaderText> <Triangle />
-            </Header>
-            <Header>
-              <HeaderText>godzina</HeaderText> <Triangle />
-            </Header>
-            <Header>
-              <HeaderText>cena</HeaderText> <Triangle />
-            </Header>
-          </Headers>
-          <VisitsListContainer variants={container} initial='hidden' animate='show'>
-            {visitsList.map((item,i) => (
-              <Visit initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: .3, delay: i * 0.2 }}
-      key={item._id} onClick={() => goToVisit(item)}>
-              <VisitContent primary>{item.usluga}</VisitContent>
-              <VisitContent >Leno Paleno</VisitContent>
-              <VisitContent >{item.data}</VisitContent>
-              <VisitContent >{item.godzina}</VisitContent>
-              <VisitContent >{item.cena}zł</VisitContent>
-              <VisitDelete onClick={() => deleteVisit(item)}><FaTrashAlt/></VisitDelete>
-          </Visit>
-            ))}
-          </VisitsListContainer>
+          {visitsList.length > 0 ? (
+            <>
+              <Headers variants={container} initial='hidden' animate='show'>
+                <Header primary>
+                  <HeaderText variants={itemOne}>usługa</HeaderText>{' '}
+                  <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>lekarz</HeaderText> <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>data</HeaderText> <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>godzina</HeaderText> <Triangle />
+                </Header>
+                <Header>
+                  <HeaderText>cena</HeaderText> <Triangle />
+                </Header>
+              </Headers>
+              <VisitsListContainer
+                variants={container}
+                initial='hidden'
+                animate='show'
+              >
+                {visitsList.map((item, i) => (
+                  <Visit
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.3, delay: i * 0.2 }}
+                    key={item._id}
+                    onClick={() => goToVisit(item)}
+                  >
+                    <VisitContent primary>{item.usluga}</VisitContent>
+                    <VisitContent>Leno Paleno</VisitContent>
+                    <VisitContent>{item.data}</VisitContent>
+                    <VisitContent>{item.godzina}:00</VisitContent>
+                    <VisitContent>{item.cena}zł</VisitContent>
+                    <VisitDelete onClick={() => deleteVisit(item)}>
+                      <FaTrashAlt />
+                    </VisitDelete>
+                  </Visit>
+                ))}
+              </VisitsListContainer>
+            </>
+          ) : (
+            <VisitsPageTitle
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.1 }}
+            >
+              Brak aktualnych wizyt
+            </VisitsPageTitle>
+          )}
         </VisitsContainer>
       </VisitsPageContainer>
       <Pattern
-          src="Pattern.png"
-          top={"12%"}
-          left={"70%"}
-          transition={{ duration: .5 }}
-          initial={{ y: -400 }}
-          animate={{ y: 0 }}
-        />
-        <Pattern
-          src="Pattern.png"
-          top={"70%"}
-          left={"13%"}
-          transition={{ duration: 1 }}
-          initial={{ y: 400 }}
-          animate={{ y: 0 }}
-        />
+        src='Pattern.png'
+        top={'12%'}
+        left={'70%'}
+        transition={{ duration: 0.5 }}
+        initial={{ y: -400 }}
+        animate={{ y: 0 }}
+      />
+      <Pattern
+        src='Pattern.png'
+        top={'70%'}
+        left={'13%'}
+        transition={{ duration: 1 }}
+        initial={{ y: 400 }}
+        animate={{ y: 0 }}
+      />
     </PageWrapper>
-  );
-};
+  )
+}
 
-export default VisitsPage;
+export default VisitsPage
