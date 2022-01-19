2cbf6e9ce3068441bf8b4b44cc4bcd3e78d74fb0
maciejsulenta <maciej.sulenta@gmail.com>
2022-01-19 09:14:14 +0100

    modal fix

--- frontend/src/pages/VisitPage/VisitPageElements.js
+++ frontend/src/pages/VisitPage/VisitPageElements.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { motion } from 'framer-motion'
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const Container = styled.div`
   width: 80%;
@@ -7,7 +7,7 @@ export const Container = styled.div`
   margin-top: 1em;
   font-size: 22px;
   margin-left: 0.3rem;
-  font-family: 'Poppins';
+  font-family: "Poppins";
   // background-color: cadetblue;
   @media screen and (max-width: 1500px) {
     font-size: 18px;
@@ -18,52 +18,93 @@ export const Container = styled.div`
   @media screen and (max-width: 960px) {
     font-size: 10px;
   }
-`
+`;
 export const VisitTitle = styled(motion.h1)`
-color: ${props => props.primary ? "#01D4BF" : "#333"};
-font-family: 'bebas neue';
-font-size: 4.5em;
-letter-spacing: .02em;
-z-index: 98;
-`
+  color: ${(props) => (props.primary ? "#01D4BF" : "#333")};
+  font-family: "bebas neue";
+  font-size: 4.5em;
+  letter-spacing: 0.02em;
+  z-index: 98;
+`;
 export const VisitContainer = styled(motion.div)`
-background-color: #fbfbfb;
-border-radius: 15px;
-padding: 1em 2em;
-width: 70%;
-margin: 0 auto;
-z-index: 99;
-`
+  background-color: #fbfbfb;
+  border-radius: 15px;
+  padding: 1em 2em;
+  width: 70%;
+  margin: 0 auto;
+  z-index: 99;
+`;
 export const VisitTitleContainer = styled.div`
-display: flex;
-justify-content: center;
-gap: 1.5em;
-`
+  display: flex;
+  justify-content: center;
+  gap: 1.5em;
+`;
 export const VisitText = styled.p`
-margin: .2em 0;
-font-size: .8em;
-`
+  margin: 0.2em 0;
+  font-size: 0.8em;
+`;
 export const ButtonContainer = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
- @media screen and (max-width: 768px) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 768px) {
     flex-direction: column;
   }
-`
+`;
 export const ButtonVisit = styled(motion.button)`
- margin: 2em 1em 1em 1em;
- padding: 1em 3em;
- outline: none;
- border: none;
- background-color: ${props => props.primary ? "#01D4BF" : "#333"};
- color: #fff;
- font-family: "bebas neue";
- font-size: .8em;
- letter-spacing: .02em;
- white-space: nowrap;
- cursor: pointer;
-`
+  margin: 2em 1em 1em 1em;
+  padding: 1em 3em;
+  outline: none;
+  border: none;
+  background-color: ${(props) => (props.primary ? "#01D4BF" : "#333")};
+  color: #fff;
+  font-family: "bebas neue";
+  font-size: 0.8em;
+  letter-spacing: 0.02em;
+  white-space: nowrap;
+  cursor: pointer;
+`;
+export const ModalShadow = styled(motion.div)`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: rgba(3, 3, 3, 0.5);
+  z-index: 999;
+`;
+export const ModalContainer = styled(motion.div)`
+  position: relative;
+  left: 0;
+  right: 0;
+  top: 25%;
+  width: 50%;
+  height: 50%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+`;
+export const ModalText = styled.h1`
+  margin-bottom: 1em;
+  font-size: 1.5em;
+`;
+export const ModalButtonsContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2em;
+`;
+export const ModalButton = styled.div`
+  background-color: ${(props) => (props.primary ? "transparent" : "#01d4bf")};
+  border: ${(props) =>
+    props.primary ? "2px solid #333" : "2px solid transparent"};
+  padding: 0.75em 2.5em;
+  cursor: pointer;
+`;
 
 // export const EmptyListHeading = styled(Title)`
 //   font-size: 2.5rem;
--- frontend/src/pages/VisitPage/index.js
+++ frontend/src/pages/VisitPage/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useLocation, useNavigate } from 'react-router-dom'
-import VisitDataService from '../../services/visit'
-import { refreshApp } from '../../store/actions/refresh'
-import { PageWrapper } from '../../components/PageWrapper'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import VisitDataService from "../../services/visit";
+import { refreshApp } from "../../store/actions/refresh";
+import { PageWrapper } from "../../components/PageWrapper";
 import {
   Container,
   VisitContainer,
@@ -12,47 +12,52 @@ import {
   VisitText,
   ButtonContainer,
   ButtonVisit,
-} from './VisitPageElements'
-import { Pattern } from '../../components/Pattern'
-import useFetchAllUsers from '../../hooks/useFetchAllUsers'
+  ModalShadow,
+  ModalContainer,
+  ModalText,
+  ModalButtonsContainer,
+  ModalButton,
+} from "./VisitPageElements";
+import { Pattern } from "../../components/Pattern";
+import useFetchAllUsers from "../../hooks/useFetchAllUsers";
 
 const VisitPage = () => {
   let {
     state: { item: state, bRoute },
-  } = useLocation()
-  let navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { user: currentUser } = useSelector((state) => state.auth)
-  const isAdmin = currentUser.roles.includes('ROLE_ADMIN')
-  const isSpec = currentUser.roles.includes('ROLE_SPEC')
-  const [isOpen, setIsOpen] = useState(false)
-  const [actionType, setActionType] = useState('')
-  const location = useLocation()
-  const backRoute = location.pathname.split('/')[1]
-  const allUsers = useFetchAllUsers()
+  } = useLocation();
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user: currentUser } = useSelector((state) => state.auth);
+  const isAdmin = currentUser.roles.includes("ROLE_ADMIN");
+  const isSpec = currentUser.roles.includes("ROLE_SPEC");
+  const [isOpen, setIsOpen] = useState(false);
+  const [actionType, setActionType] = useState("");
+  const location = useLocation();
+  const backRoute = location.pathname.split("/")[1];
+  const allUsers = useFetchAllUsers();
 
   const onHandleAction = () => {
-    if (actionType === 'usun') {
+    if (actionType === "usun") {
       VisitDataService.remove(state.id)
         .then((response) => {
-          setIsOpen(false)
-          dispatch(refreshApp())
-          navigate('/visits')
+          setIsOpen(false);
+          dispatch(refreshApp());
+          navigate("/visits");
         })
-        .catch((e) => console.log(e))
+        .catch((e) => console.log(e));
     } else {
       let data = {
         status: !state.status,
-      }
+      };
       VisitDataService.update(state.id, data)
         .then((response) => {
-          setIsOpen(false)
-          dispatch(refreshApp())
-          navigate('/archive')
+          setIsOpen(false);
+          dispatch(refreshApp());
+          navigate("/archive");
         })
-        .catch((e) => console.log(e))
+        .catch((e) => console.log(e));
     }
-  }
+  };
 
   return (
     <PageWrapper>
@@ -95,15 +100,15 @@ const VisitPage = () => {
               <VisitText>Ulica: {state.ulica}</VisitText>
               <VisitText>Kod pocztowy: {state.kodPocztowy}</VisitText>
               <VisitText>
-                Status: {state.status === false ? 'W trakcie' : 'Zrealizowna'}
+                Status: {state.status === false ? "W trakcie" : "Zrealizowna"}
               </VisitText>
               <ButtonContainer>
                 <ButtonVisit
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                   onClick={() => {
-                    setIsOpen(true)
-                    setActionType('usun')
+                    setIsOpen(true);
+                    setActionType("usun");
                   }}
                   primary={!isAdmin && !isSpec && true}
                 >
@@ -115,13 +120,13 @@ const VisitPage = () => {
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
                     onClick={() => {
-                      setIsOpen(true)
-                      setActionType('archiwizuj')
+                      setIsOpen(true);
+                      setActionType("archiwizuj");
                     }}
                   >
                     {state.status === false
-                      ? 'Archiwizuj wizytę'
-                      : 'Uaktualnij wizyte'}
+                      ? "Archiwizuj wizytę"
+                      : "Uaktualnij wizyte"}
                   </ButtonVisit>
                 ) : null}
                 <ButtonVisit
@@ -130,107 +135,54 @@ const VisitPage = () => {
                   onClick={() => navigate(`/${bRoute ? bRoute : backRoute}`)}
                 >
                   {bRoute
-                    ? 'Wróć do ustawień konta'
+                    ? "Wróć do ustawień konta"
                     : state.status === false
-                    ? 'Wróć do aktualnych wizyt'
-                    : 'Wróć do archiwum wizyt'}
+                    ? "Wróć do aktualnych wizyt"
+                    : "Wróć do archiwum wizyt"}
                 </ButtonVisit>
               </ButtonContainer>
             </VisitContainer>
             {isOpen && (
-              <div
-                style={{
-                  width: '100vw',
-                  height: '100vh',
-                  position: 'absolute',
-                  left: '0',
-                  top: '0',
-                  backgroundColor: 'rgba(3,3,3,.5)',
-                  zIndex: '999',
-                }}
-              >
-                <div
-                  style={{
-                    position: 'relative',
-                    width: '50%',
-                    height: '50%',
-                    backgroundColor: '#fff',
-                    left: '0',
-                    right: '0',
-                    top: '25%',
-                    margin: 'auto',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <h2 style={{ marginBottom: '20px' }}>
-                    {actionType === 'usun'
-                      ? 'Na pewno chcesz usunąć wizytę?'
-                      : actionType === 'archiwizuj'
-                      ? 'Na pewno chcesz zarchiwizowac wizytę?'
+              <ModalShadow>
+                <ModalContainer>
+                  <ModalText>
+                    {actionType === "usun"
+                      ? "Na pewno chcesz usunąć wizytę?"
+                      : actionType === "archiwizuj"
+                      ? "Na pewno chcesz zarchiwizowac wizytę?"
                       : null}
-                  </h2>
-                  <div
-                    style={{
-                      position: 'relative',
-                      backgroundColor: '#fff',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <button
-                      style={{
-                        backgroundColor: 'transparent',
-                        border: '2px solid #333',
-                        padding: '.75em 50px',
-                        marginRight: '5px',
-                        cursor: 'pointer',
-                      }}
-                      onClick={() => setIsOpen(false)}
-                    >
+                  </ModalText>
+                  <ModalButtonsContainer>
+                    <ModalButton primary onClick={() => setIsOpen(false)}>
                       Nie
-                    </button>
-                    <button
-                      style={{
-                        backgroundColor: '#01d4bf',
-                        border: '2px solid transparent',
-                        padding: '.75em 50px',
-                        marginLeft: '5px',
-                        cursor: 'pointer',
-                      }}
-                      onClick={onHandleAction}
-                    >
-                      Tak
-                    </button>
-                  </div>
-                </div>
-              </div>
+                    </ModalButton>
+                    <ModalButton onClick={onHandleAction}>Tak</ModalButton>
+                  </ModalButtonsContainer>
+                </ModalContainer>
+              </ModalShadow>
             )}
           </>
         )}
       </Container>
 
       <Pattern
-        src='/pattern.png'
-        top='50%'
-        left='18%'
+        src="/pattern.png"
+        top="50%"
+        left="18%"
         initial={{ opacity: 0, x: 200 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.5 }}
       />
       <Pattern
-        src='/pattern.png'
-        top='35%'
-        left='65%'
+        src="/pattern.png"
+        top="35%"
+        left="65%"
         initial={{ opacity: 0, x: -200 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.5 }}
       />
     </PageWrapper>
-  )
-}
+  );
+};
 
-export default VisitPage
+export default VisitPage;
