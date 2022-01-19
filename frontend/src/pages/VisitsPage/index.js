ae05274d0618a692acc6777a6b8eb5344d265434
maciejsulenta <maciej.sulenta@gmail.com>
2022-01-19 09:40:40 +0100

    modal fix

--- frontend/src/pages/VisitsPage/index.js
+++ frontend/src/pages/VisitsPage/index.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import VisitDataService from '../../services/visit'
-import { useSelector, useDispatch } from 'react-redux'
-import { refreshApp } from '../../store/actions/refresh'
-import { useNavigate } from 'react-router-dom'
-import { PageWrapper } from '../../components/PageWrapper'
-import { FaTrashAlt } from 'react-icons/fa'
-import { Pattern } from '../../components/Pattern'
+import React, { useEffect, useState } from "react";
+import VisitDataService from "../../services/visit";
+import { useSelector, useDispatch } from "react-redux";
+import { refreshApp } from "../../store/actions/refresh";
+import { useNavigate } from "react-router-dom";
+import { PageWrapper } from "../../components/PageWrapper";
+import { FaTrashAlt } from "react-icons/fa";
+import { Pattern } from "../../components/Pattern";
 
 import {
   VisitsPageContainer,
@@ -23,8 +23,15 @@ import {
   VisitContent,
   VisitDelete,
   MyPaginate,
-} from './VisitsPageElements'
-import useFetchAllUsers from '../../hooks/useFetchAllUsers'
+} from "./VisitsPageElements";
+import {
+  ModalShadow,
+  ModalContainer,
+  ModalText,
+  ModalButtonsContainer,
+  ModalButton,
+} from "../VisitPage/VisitPageElements";
+import useFetchAllUsers from "../../hooks/useFetchAllUsers";
 
 const VisitsPage = () => {
   const [filterPosition, setFilterPosition] = useState({
@@ -33,59 +40,59 @@ const VisitsPage = () => {
     data: 0,
     godzina: 0,
     cena: 0,
-  })
-  const [visitsList, setVisitsList] = useState([])
-  const [isDelete, setIsDelete] = useState(false)
-  const [visitId, setVisitId] = useState('')
-  const [pageNumber, setPageNumber] = useState(0)
-  const { user: currentUser } = useSelector((state) => state.auth)
-  const { refresh: isRefresh } = useSelector((state) => state)
-  const dispatch = useDispatch()
-  let navigate = useNavigate()
-  const allUsers = useFetchAllUsers()
-  const visitsPerPage = 5
-  const pagesVisited = pageNumber * visitsPerPage
+  });
+  const [visitsList, setVisitsList] = useState([]);
+  const [isDelete, setIsDelete] = useState(false);
+  const [visitId, setVisitId] = useState("");
+  const [pageNumber, setPageNumber] = useState(0);
+  const { user: currentUser } = useSelector((state) => state.auth);
+  const { refresh: isRefresh } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+  const allUsers = useFetchAllUsers();
+  const visitsPerPage = 5;
+  const pagesVisited = pageNumber * visitsPerPage;
 
   useEffect(() => {
-    retrieveVisits()
-  }, [isRefresh])
+    retrieveVisits();
+  }, [isRefresh]);
 
   const retrieveVisits = () => {
     VisitDataService.getAll()
       .then((response) => {
-        const visitsArr = response.data.filter((item) => item.status === false)
-        if (currentUser.roles.includes('ROLE_ADMIN')) {
-          setVisitsList(visitsArr)
-        } else if (currentUser.roles.includes('ROLE_SPEC')) {
+        const visitsArr = response.data.filter((item) => item.status === false);
+        if (currentUser.roles.includes("ROLE_ADMIN")) {
+          setVisitsList(visitsArr);
+        } else if (currentUser.roles.includes("ROLE_SPEC")) {
           const specificDoctorVisits = visitsArr.filter(
             (visit) => visit.specjalista.sid === currentUser.id
-          )
-          setVisitsList(specificDoctorVisits)
+          );
+          setVisitsList(specificDoctorVisits);
         } else {
           const userVisitsArr = visitsArr.filter(
             (visit) => visit.email === currentUser.email
-          )
-          setVisitsList(userVisitsArr)
+          );
+          setVisitsList(userVisitsArr);
         }
       })
       .catch((e) => {
-        console.log(e)
-      })
-  }
+        console.log(e);
+      });
+  };
 
   const goToVisit = (item) => {
-    navigate(`/visits/${item.id}`, { state: { item } })
-  }
+    navigate(`/visits/${item.id}`, { state: { item } });
+  };
 
   const onVisitDelete = () => {
-    setIsDelete(false)
+    setIsDelete(false);
     VisitDataService.remove(visitId.id)
       .then((response) => {
-        console.log('Usunieto wizyte pomyslnie!')
-        dispatch(refreshApp())
+        console.log("Usunieto wizyte pomyslnie!");
+        dispatch(refreshApp());
       })
-      .catch((e) => console.log(e))
-  }
+      .catch((e) => console.log(e));
+  };
 
   const displayVisits = visitsList
     .slice(pagesVisited, pagesVisited + visitsPerPage)
@@ -109,42 +116,42 @@ const VisitsPage = () => {
               <VisitContent>{visit.cena}zł</VisitContent>
               <VisitDelete
                 onClick={(e) => {
-                  e.stopPropagation()
-                  setIsDelete(true)
-                  setVisitId(visit)
+                  e.stopPropagation();
+                  setIsDelete(true);
+                  setVisitId(visit);
                 }}
               >
                 <FaTrashAlt />
               </VisitDelete>
             </Visit>
           </>
-        )
+        );
       }
-    })
+    });
 
-  const pageCount = Math.ceil(visitsList.length / visitsPerPage)
+  const pageCount = Math.ceil(visitsList.length / visitsPerPage);
   const changePage = ({ selected }) => {
-    setPageNumber(selected)
-  }
+    setPageNumber(selected);
+  };
 
   const onFilterByService = () => {
     if (filterPosition.usluga === 0) {
       const descArr = visitsList.sort((a, b) =>
         b.usluga.toLowerCase() > a.usluga.toLowerCase() ? 1 : -1
-      )
-      setVisitsList(descArr)
-      setFilterPosition({ usluga: 1, data: 0, godzina: 0, cena: 0, lekarz: 0 })
+      );
+      setVisitsList(descArr);
+      setFilterPosition({ usluga: 1, data: 0, godzina: 0, cena: 0, lekarz: 0 });
     } else if (filterPosition.usluga === 1) {
       const ascArr = visitsList.sort((a, b) =>
         a.usluga.toLowerCase() > b.usluga.toLowerCase() ? 1 : -1
-      )
-      setVisitsList(ascArr)
-      setFilterPosition({ usluga: 2, data: 0, godzina: 0, cena: 0, lekarz: 0 })
+      );
+      setVisitsList(ascArr);
+      setFilterPosition({ usluga: 2, data: 0, godzina: 0, cena: 0, lekarz: 0 });
     } else if (filterPosition.usluga === 2) {
-      retrieveVisits()
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 })
+      retrieveVisits();
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 });
     }
-  }
+  };
 
   const onFilterBySpecialist = () => {
     if (filterPosition.lekarz === 0) {
@@ -153,76 +160,76 @@ const VisitsPage = () => {
         a.specjalista.nazwisko.toLowerCase()
           ? 1
           : -1
-      )
-      setVisitsList(descArr)
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 1 })
+      );
+      setVisitsList(descArr);
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 1 });
     } else if (filterPosition.lekarz === 1) {
       const ascArr = visitsList.sort((a, b) =>
         a.specjalista.nazwisko.toLowerCase() >
         b.specjalista.nazwisko.toLowerCase()
           ? 1
           : -1
-      )
-      setVisitsList(ascArr)
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 2 })
+      );
+      setVisitsList(ascArr);
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 2 });
     } else if (filterPosition.lekarz === 2) {
-      retrieveVisits()
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 })
+      retrieveVisits();
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 });
     }
-  }
+  };
 
   const onFilterByDate = () => {
     if (filterPosition.data === 0) {
       const descArr = visitsList.sort((a, b) => {
-        let aa = a.data.split('.').reverse().join()
-        let bb = b.data.split('.').reverse().join()
-        return aa > bb ? -1 : aa > bb ? 1 : 0
-      })
-      setVisitsList(descArr)
-      setFilterPosition({ usluga: 0, data: 1, godzina: 0, cena: 0, lekarz: 0 })
+        let aa = a.data.split(".").reverse().join();
+        let bb = b.data.split(".").reverse().join();
+        return aa > bb ? -1 : aa > bb ? 1 : 0;
+      });
+      setVisitsList(descArr);
+      setFilterPosition({ usluga: 0, data: 1, godzina: 0, cena: 0, lekarz: 0 });
     } else if (filterPosition.data === 1) {
       const ascArr = visitsList.sort((a, b) => {
-        let aa = a.data.split('.').reverse().join()
-        let bb = b.data.split('.').reverse().join()
-        return aa < bb ? -1 : aa > bb ? 1 : 0
-      })
-      setVisitsList(ascArr)
-      setFilterPosition({ usluga: 0, data: 2, godzina: 0, cena: 0, lekarz: 0 })
+        let aa = a.data.split(".").reverse().join();
+        let bb = b.data.split(".").reverse().join();
+        return aa < bb ? -1 : aa > bb ? 1 : 0;
+      });
+      setVisitsList(ascArr);
+      setFilterPosition({ usluga: 0, data: 2, godzina: 0, cena: 0, lekarz: 0 });
     } else if (filterPosition.data === 2) {
-      retrieveVisits()
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 })
+      retrieveVisits();
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 });
     }
-  }
+  };
 
   const onFilterByHour = () => {
     if (filterPosition.godzina === 0) {
-      const descArr = visitsList.sort((a, b) => b.godzina - a.godzina)
-      setVisitsList(descArr)
-      setFilterPosition({ usluga: 0, data: 0, godzina: 1, cena: 0, lekarz: 0 })
+      const descArr = visitsList.sort((a, b) => b.godzina - a.godzina);
+      setVisitsList(descArr);
+      setFilterPosition({ usluga: 0, data: 0, godzina: 1, cena: 0, lekarz: 0 });
     } else if (filterPosition.godzina === 1) {
-      const ascArr = visitsList.sort((a, b) => a.godzina - b.godzina)
-      setVisitsList(ascArr)
-      setFilterPosition({ usluga: 0, data: 0, godzina: 2, cena: 0, lekarz: 0 })
+      const ascArr = visitsList.sort((a, b) => a.godzina - b.godzina);
+      setVisitsList(ascArr);
+      setFilterPosition({ usluga: 0, data: 0, godzina: 2, cena: 0, lekarz: 0 });
     } else if (filterPosition.godzina === 2) {
-      retrieveVisits()
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 })
+      retrieveVisits();
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 });
     }
-  }
+  };
 
   const onFilterByPrice = () => {
     if (filterPosition.cena === 0) {
-      const descArr = visitsList.sort((a, b) => b.cena - a.cena)
-      setVisitsList(descArr)
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 1, lekarz: 0 })
+      const descArr = visitsList.sort((a, b) => b.cena - a.cena);
+      setVisitsList(descArr);
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 1, lekarz: 0 });
     } else if (filterPosition.cena === 1) {
-      const ascArr = visitsList.sort((a, b) => a.cena - b.cena)
-      setVisitsList(ascArr)
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 2, lekarz: 0 })
+      const ascArr = visitsList.sort((a, b) => a.cena - b.cena);
+      setVisitsList(ascArr);
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 2, lekarz: 0 });
     } else if (filterPosition.cena === 2) {
-      retrieveVisits()
-      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 })
+      retrieveVisits();
+      setFilterPosition({ usluga: 0, data: 0, godzina: 0, cena: 0, lekarz: 0 });
     }
-  }
+  };
 
   const container = {
     hidden: { opacity: 0 },
@@ -234,14 +241,14 @@ const VisitsPage = () => {
         staggerChildren: 1,
       },
     },
-  }
+  };
   const itemOne = {
     hidden: { y: -100, opacity: 0 },
     visible: {
       y: 0,
       opacity: 1,
     },
-  }
+  };
   return (
     <PageWrapper>
       <VisitsPageContainer>
@@ -265,7 +272,7 @@ const VisitsPage = () => {
         <VisitsContainer>
           {visitsList.length > 0 ? (
             <>
-              <Headers variants={container} initial='hidden' animate='show'>
+              <Headers variants={container} initial="hidden" animate="show">
                 <Header primary onClick={onFilterByService}>
                   <HeaderText variants={itemOne}>usługa</HeaderText>
                   {filterPosition.usluga === 0 ? (
@@ -319,13 +326,13 @@ const VisitsPage = () => {
               </Headers>
               <VisitsListContainer
                 variants={container}
-                initial='hidden'
-                animate='show'
+                initial="hidden"
+                animate="show"
               >
                 {displayVisits}
                 <MyPaginate
-                  previousLabel={'Poprzednia strona'}
-                  nextLabel={'Następna strona'}
+                  previousLabel={"Poprzednia strona"}
+                  nextLabel={"Następna strona"}
                   pageCount={pageCount}
                   onPageChange={changePage}
                 />
@@ -343,92 +350,37 @@ const VisitsPage = () => {
         </VisitsContainer>
       </VisitsPageContainer>
       {isDelete && (
-        <div
-          style={{
-            width: '100vw',
-            height: '100vh',
-            position: 'absolute',
-            left: '0',
-            top: '0',
-            backgroundColor: 'rgba(3,3,3,.5)',
-            zIndex: '999',
-          }}
-        >
-          <div
-            style={{
-              position: 'relative',
-              width: '50%',
-              height: '50%',
-              backgroundColor: '#fff',
-              left: '0',
-              right: '0',
-              top: '25%',
-              margin: 'auto',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <h2 style={{ marginBottom: '20px' }}>
+        <ModalShadow>
+          <ModalContainer>
+            <ModalText>
               Na pewno chcesz usunąć wizytę?
-            </h2>
-            <div
-              style={{
-                position: 'relative',
-                backgroundColor: '#fff',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <button
-                style={{
-                  backgroundColor: 'transparent',
-                  border: '2px solid #333',
-                  padding: '.75em 50px',
-                  marginRight: '5px',
-                  cursor: 'pointer',
-                }}
-                onClick={() => setIsDelete(false)}
-              >
-                Nie
-              </button>
-              <button
-                style={{
-                  backgroundColor: '#01d4bf',
-                  border: '2px solid transparent',
-                  padding: '.75em 50px',
-                  marginLeft: '5px',
-                  cursor: 'pointer',
-                }}
-                onClick={onVisitDelete}
-              >
-                Tak
-              </button>
-            </div>
-          </div>
-        </div>
+            </ModalText>
+            <ModalButtonsContainer>
+              <ModalButton primary onClick={() => setIsDelete(false)}>Nie</ModalButton>
+              <ModalButton onClick={onVisitDelete}>Tak</ModalButton>
+            </ModalButtonsContainer>
+          </ModalContainer>
+        </ModalShadow>
       )}
 
       <Pattern
-        src='Pattern.png'
-        top={'12%'}
-        left={'70%'}
+        src="Pattern.png"
+        top={"12%"}
+        left={"70%"}
         transition={{ duration: 0.5 }}
         initial={{ y: -400 }}
         animate={{ y: 0 }}
       />
       <Pattern
-        src='Pattern.png'
-        top={'70%'}
-        left={'13%'}
+        src="Pattern.png"
+        top={"70%"}
+        left={"13%"}
         transition={{ duration: 1 }}
         initial={{ y: 400 }}
         animate={{ y: 0 }}
       />
     </PageWrapper>
-  )
-}
+  );
+};
 
-export default VisitsPage
+export default VisitsPage;
